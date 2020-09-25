a45ab67c8ea5113ecd8d7b86daca6ccab14c43c1
Shrestha-Binod <shrbinod@sheridancollege.ca>
2020-09-25 17:18:01 -0400

    extension function added

--- ExtensionsPractice.kt
+++ ExtensionsPractice.kt
@@ -1,2 +1,18 @@
-class ExtensionsPractice {
+
+fun main(args:Array<String>){
+    var studentOne = Student()
+    println("Passed: "+studentOne.hasPassed(55))
+    println("Scholarship: "+studentOne.gotScholarShip(98))
+
+
+}
+
+fun Student.gotScholarShip(marks: Int): Boolean{ // this class injection provides the extension
+    return marks > 90
+}
+
+class Student {
+    fun hasPassed(marks:Int): Boolean {
+        return marks > 50
+    }
 }
